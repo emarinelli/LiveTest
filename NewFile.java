70518c5af558330203a443b931a26d51bcf8a1ad
krysiak2000 <krysia.k2000@gmail.com>
2013-02-28 18:49:31 -0800

    Update NewFile.java

--- NewFile.java
+++ NewFile.java
@@ -1,6 +1,7 @@
 public class NewFile{
 	public static void main (String[] args){
 		System.out.println("Here files be");
+		System.out.println("This line added at 6:49pm Thursday - added by copy paste of whole program");
 		System.out.println("Changed from original");
 		System.out.println("This line added at 6:43pm Thursday");
 	}
