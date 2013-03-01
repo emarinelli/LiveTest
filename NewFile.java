3c0dde9e36b59934f4933e1add7003953b21de3e
krysiak2000 <krysia.k2000@gmail.com>
2013-02-28 18:43:16 -0800

    Update NewFile.java

--- NewFile.java
+++ NewFile.java
@@ -2,5 +2,6 @@ public class NewFile{
 	public static void main (String[] args){
 		System.out.println("Here files be");
 		System.out.println("Changed from original");
+		System.out.println("This line added at 6:43pm Thursday");
 	}
 }
